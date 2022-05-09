6f8c13b723fdb16a3bc755019280645350071ce9
karthikq <kk968346@gmail.com>
2022-05-09 13:57:08 +0530

    testing contollers45

--- utils/config.js
+++ utils/config.js
@@ -4,7 +4,7 @@ module.exports = {
   port: 3000,
   db: {
     production: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.44gx5.mongodb.net/${process.env.MONGO_DB}`,
-    development: process.env.MONGO_URL,
+    development: "mongodb://localhost/test",
     test: process.env.MONGO_URL,
   },
 };
