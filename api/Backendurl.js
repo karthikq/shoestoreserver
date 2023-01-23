d1be0dc27b23847c56a1ef0b17fa1d3fd1e0b081
karthikq <kk968346@gmail.com>
2023-01-23 20:58:29 +0530

    cg

--- api/Backendurl.js
+++ api/Backendurl.js
@@ -4,6 +4,6 @@ exports.backendUrl = () => {
   if (process.env.NODE_ENV === "production") {
     return "https://shoestoreserver-production.up.railway.app/v1";
   } else {
-    return "http://localhost:5000";
+    return "http://localhost:5001";
   }
 };
